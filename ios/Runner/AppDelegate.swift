4fcb8bc8c24c57e80792c38b9759e46327033479
Muhamamd Jawad <m.jawad8991@gmail.com>
2022-02-23 12:47:52 +0500

    ios method channel added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,21 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      
+      let channelName = "flutter.native/helper"
+      let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      
+      let methodChannel = FlutterMethodChannel(name: channelName, binaryMessenger: controller.binaryMessenger)
+
+      methodChannel.setMethodCallHandler {(call: FlutterMethodCall, result: FlutterResult) -> Void in
+          if (call.method == "startScreenRecord") {
+              print("start recording");
+              result("Start Recording");
+          }else if(call.method == "stopScreenRecord"){
+              print("stop recording");
+              result("Stop Recording");
+          }
+      }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
